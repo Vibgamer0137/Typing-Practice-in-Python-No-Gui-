47c2c55c46a43a28b8e544d961d9e37e595391e9
Vibhav <vibhav.programmer@gmail.com>
2026-06-20 17:59:31 +0600

    Added first option practice a word function

--- Database_for_main/main_controls.py
+++ Database_for_main/main_controls.py
@@ -1,9 +1,22 @@
+import time as t
 class main:
-    def display_menu():
+    def display_menu(self):
         print("----------------Typing-Practice-------------------")
         print("---------------1.Practice a word------------------")
         print("----2.Practice a word saved(still developing)-----")
         print("-------------------3.Exit-------------------------")
         menu_choice =  input("Choose ethier 1 nor 2 nor 3:")
         return menu_choice
-    
+    def practice_a_word(self, word, times):
+        times = times+1
+        for i in range(1, times):
+            a = input(f"Enter {word}: ")
+            while a==word:
+                print("Succesfull")
+                a = input(f"Enter {word}: ")
+            else:
+                print(f"You entered wrong word:{a}")
+                print("Retrying")
+                t.sleep(5)
+                a = input(f"Enter {word}: ")
+        print("Completed")
