5cf0219d5156ff1060d4f80c7a2c003fcdb73911
Vibhav <vibhav.programmer@gmail.com>
2026-06-20 16:59:07 +0600

    first commit

---
+++ Database_for_main/main_controls.py
@@ -0,0 +1,9 @@
+class main:
+    def display_menu():
+        print("----------------Typing-Practice-------------------")
+        print("---------------1.Practice a word------------------")
+        print("----2.Practice a word saved(still developing)-----")
+        print("-------------------3.Exit-------------------------")
+        menu_choice =  input("Choose ethier 1 nor 2 nor 3:")
+        return menu_choice
+    
