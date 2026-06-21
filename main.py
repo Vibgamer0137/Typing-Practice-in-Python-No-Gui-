b91f25a92160bc806290c1e23082134834ee1142
Vibhav <vibhav.programmer@gmail.com>
2026-06-21 12:44:15 +0600

    Done-with-first-version

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from Database_for_main.main_controls import code
 while True:
     user_input = code.display_menu()
     if user_input == 1:
-        pass
+        word = input("Write a word:")
+        times = int(input("How many times:"))
+        code.practice_a_word(word, times)
     elif user_input == 2:
-        pass
+        code.practice_a_word_saved()
     elif user_input == 3:
-        pass
+        code.exit()
     else:
-        pass 
+        print("Try again")
