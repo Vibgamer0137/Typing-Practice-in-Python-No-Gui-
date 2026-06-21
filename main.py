369b8474de6cac646092c682dff895b978bbd409
Vibhav <vibhav.programmer@gmail.com>
2026-06-21 13:57:41 +0600

    Fixed Bug:not converted to int for main.py

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from Database_for_main.main_controls import code
 while True:
     user_input = code.display_menu()
     if user_input == 1:
-        word = input("Write a word:")
-        times = int(input("How many times:"))
-        code.practice_a_word(word, times)
+        words = input("Write a word:")
+        timess = int(input("How many times:"))
+        code.practice_a_word(words, timess)
+        break
     elif user_input == 2:
         code.practice_a_word_saved()
+        break
     elif user_input == 3:
         code.exit()
+        break
     else:
         print("Try again")
