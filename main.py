2e84d6180b0d5ba7636c8e6668721811292234ba
Vibhav <vibhav.programmer@gmail.com>
2026-06-21 12:15:02 +0600

    Added-retrying-logic-still-not-using

---
+++ main.py
@@ -0,0 +1,11 @@
+from Database_for_main.main_controls import code
+while True:
+    user_input = code.display_menu()
+    if user_input == 1:
+        pass
+    elif user_input == 2:
+        pass
+    elif user_input == 3:
+        pass
+    else:
+        pass 
